deaaeeddc233827dfe6107bbd808f8f3fd47bdfa
Gabriel Pagote <gabriielpagote@gmail.com>
2021-05-13 15:32:43 -0300

    .cpp -> .c; lista 4 iniciada

---
+++ Lista 4 em C/L04E03.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+// exercicio 3 - lista 4
+
+int main ()
+{
+	float num1, num2, num3;
+	printf("\nInsira tres numeros: \n");
+	scanf("%f%f%f", &num1, num2, num3, pri, seg, ter);
+	if(num1>num2)
+	{
+		if(num1>num3)
+		{
+			num1= pri
+		}
+		else
+			if(num3>num2)
+			{
+				num3 = pri	
+			}
+			
+	}
+	else
+		if(num2>num3)
+		{
+			num2 = pri
+		}
+}
